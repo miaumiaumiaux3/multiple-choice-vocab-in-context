49d2ed6eaffd75c31fc91e19b774fd6ac8efc737
miaumiaumiaux3 <tifachase@gmail.com>
2024-05-06 15:05:25 +0200

    created separate files for generating polish and english sentences

---
+++ generate_sentence_en.py
@@ -0,0 +1,50 @@
+###################
+# https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF
+# Required downloads and dependencies
+# pip3 install huggingface-hub
+# pip install llama-cpp-python
+# huggingface-cli download TheBloke/Mistral-7B-Instruct-v0.2-GGUF mistral-7b-instruct-v0.2.Q4_K_M.gguf --local-dir . --local-dir-use-symlinks False
+#################
+
+from llama_cpp import Llama
+
+def load_model():
+    llm = Llama(
+        model_path="./mistral-7b-instruct-v0.2.Q4_K_M.gguf",  # Download the model file first
+        #chat_format= "llama-2", # The chat format to use - either "llama-1" or "llama-2" -- CAN BE OMITTED FOR DEFAULT FLAVOR
+        n_ctx=32768,  # The max sequence length to use - note that longer sequence lengths require much more resources
+        n_threads=8,            # The number of CPU threads to use, tailor to your system and the resulting performance
+        n_gpu_layers=35         # The number of layers to offload to GPU, if you have GPU acceleration available
+    )
+    return llm
+
+def prompt_model(llm, human_prompt, t = 120) -> str:
+    output = llm(
+        f"<s>[INST] {human_prompt} [/INST]", # Prompt
+        max_tokens=120,  # Generate up to 120 tokens
+        stop=["</s>"],   # Example stop token - not necessarily correct for this specific model! Please check before using.
+        echo=False        # Whether to echo the prompt
+    )
+    return(output["choices"][0]["text"])
+
+#For running as a solo script to play with a model
+if __name__ == "__main__":
+    # print(f"CUDA is available: {torch.cuda.is_available()}")
+    # print(f"CUDA device count: {torch.cuda.device_count()}")
+    # print(f"CUDA device name: {torch.cuda.get_device_name(0)}")
+    # print(f"CUDA device index: {torch.cuda.current_device()}")
+    # print(f"CUDA device: {torch.cuda.get_device_properties(0)}")
+    # print(f"PyTorch version: {torch.__version__}")
+    print("------------------------------------------------------------")
+
+    llm = load_model()
+
+    while True:
+        human_prompt = input("Enter a prompt:\n")
+        if human_prompt.lower() == "exit":
+            exit()
+        if not human_prompt:
+            print("You wrote nothing, so I'm assuming you're done for now, goodbye!")
+            exit()
+        result = prompt_model(llm, human_prompt)
+        print(result)
---
+++ generate_sentence_pl.py
@@ -0,0 +1,66 @@
+#############################################
+# Bielik-7B-Instruct-v0.1-GPTQ
+# https://colab.research.google.com/drive/1Al9glPVCuOXbtDsks8cMcuzkuu8YDzpg?usp=sharing#scrollTo=Jr2DiK6PLHdQ
+#
+# Required installs and dependencies:
+# pip install optimum -q
+# pip install auto-gptq -q
+# pip install ctransformers[cuda]
+######################################
+
+import torch
+import warnings
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+
+def load_polish_model():
+    model_id = "speakleash/Bielik-7B-Instruct-v0.1-GPTQ"
+    tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        device_map="auto",
+        trust_remote_code=False,
+        revision="main"
+    )
+    pipe = pipeline(model=model, tokenizer=tokenizer, task='text-generation', return_full_text=False)
+    gptq = True #should this be global?
+
+    return pipe
+
+def prompt_polish_model(pipe, human_prompt, t = 120) -> str:
+    prompt = f"<s>[INST]{human_prompt}[/INST]"
+    outputs = pipe(
+        prompt,
+        max_new_tokens=t,
+        do_sample=True,
+        temperature=0.1, #0.1 is the default, changing it to .5 leads to... German xD
+        top_p=0.95,
+    )
+    return outputs[0]["generated_text"]
+
+
+#For running as a solo script to play with a model
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore") #suppress warnings, since the output gets MESSY, comment out to debug issues
+
+    print(f"CUDA is available: {torch.cuda.is_available()}")
+    print(f"CUDA device count: {torch.cuda.device_count()}")
+    print(f"CUDA device name: {torch.cuda.get_device_name(0)}")
+    print(f"CUDA device index: {torch.cuda.current_device()}")
+    print(f"CUDA device: {torch.cuda.get_device_properties(0)}")
+    print(f"PyTorch version: {torch.__version__}")
+    print("------------------------------------------------------------")
+
+    pipe = load_polish_model()
+    gptq = True
+
+    while True:
+        human_prompt = input("Enter a prompt:\n")
+        if human_prompt.lower() == "exit":
+            exit()
+        if not human_prompt:
+            print("You wrote nothing, so I'm assuming you're done for now, goodbye!")
+            exit()
+        result = prompt_polish_model(pipe, human_prompt)
+        print(result)
