ed3d33e655482bb21f51790a9cef0ad5f51d4587
miaumiaumiaux3 <tifachase@gmail.com>
2024-05-12 00:41:13 +0200

    changed temperature to 0.7 and top_p to .99 to better match Mistral's defaults, also it's way nicer imho

--- generate_sentence_pl.py
+++ generate_sentence_pl.py
@@ -1,5 +1,5 @@
 #############################################
-# Bielik-7B-Instruct-v0.1-GPTQ
+# Bielik-7B-Instruct-v0.1-GGUF
 # https://colab.research.google.com/drive/1Al9glPVCuOXbtDsks8cMcuzkuu8YDzpg?usp=sharing#scrollTo=Jr2DiK6PLHdQ
 #
 # Required installs and dependencies:
@@ -30,20 +30,27 @@ def load_model():
 
     return pipe
 
-def prompt_model(pipe, human_prompt, temp=0.3) -> str:
+def prompt_model(pipe, human_prompt, temp=0.7, topp = 0.99) -> str:
     prompt = f"<s>[INST]{human_prompt}[/INST]"
     outputs = pipe(
         prompt,
         max_new_tokens=120,
-        temperature=temp, #0.1 is the default, changing it to 0.5 has given German output
+        temperature=temp, #0.7 is mistral default, 0.1 was gptQchanging it to 0.5 has given German output
         do_sample=True, #gives more variety in the output versus greedy decoding
-        top_p=1, #top probability to sample from, default is 1, but 0.95 was reccommended in GPTQ setting, 1 is fine for now
+        top_p=topp, #top probability to sample from, default is 1, but 0.95 was reccommended in GPTQ setting
+        # .99 is my current favorite
     )
     result = outputs[0]["generated_text"]
     return result.strip(' "\'\t\r\n')
 
 def generate_sample_sentence(pipe, word):
-    built_prompt = f"Napisz zdanie, w którym się pojawia słowo {word}."
+    built_prompt = f"Napisz zdanie, które zawiera słowo {word}."
+    #built_prompt = f"Napisz zaskakujące zdanie, które zawiera słowo {word}." #has interesting results, actually
+    #built_prompt = f"Napisz kreatywne zdanie, które zawiera słowo {word}."
+    #built_prompt = f"Napisz pierwsze zdanie opowieści, które zawiera słowo '{word}' i wprowadza poczucie tajemnicy."
+    #built_prompt = f"Napisz pierwsze zdanie opowieści, które zawiera słowo '{word}' i wprowadza poczucie przygody."
+    #built_prompt = f"Napisz pierwsze zdanie opowieści, które zawiera słowo '{word}' i wprowadza poczucie napięcia."
+    #built_prompt = f"Napisz zdanie, w którym się pojawia słowo {word}."
     result = prompt_model(pipe, built_prompt)
     return result
 
@@ -68,6 +75,8 @@ if __name__ == "__main__":
     pipe = load_model()
 
     # while True:
+    #     #start timer
+    #     start_time = time.monotonic()
     #     human_prompt = input("Enter a prompt:\n")
     #     if human_prompt.lower() == "exit":
     #         exit()
@@ -76,16 +85,19 @@ if __name__ == "__main__":
     #         exit()
     #     result = prompt_model(pipe, human_prompt)
     #     print(result)
+    #     #end timer
+    #     end_time = time.monotonic()
+    #     print('Runtime: ' + str(timedelta(seconds=end_time - start_time)))
     #
 
 
     while True:
         #start timer
         start_time = time.monotonic()
-        human_prompt = "Napisz zdanie, w którym się pojawia słowo tańczę"
+        human_prompt = "Napisz zdanie, które zawiera słowo taniec."
         #human_prompt = "Napisz zdanie, w którym znajduje się słowa tańczyć."
         #human_prompt = "Napisz zdanie, używając słowa tańczyć."
-        result = prompt_model(pipe, human_prompt, temp=0.3)
+        result = prompt_model(pipe, human_prompt, temp=0.7, topp=.99)
         print(result)
         #end timer
         end_time = time.monotonic()
