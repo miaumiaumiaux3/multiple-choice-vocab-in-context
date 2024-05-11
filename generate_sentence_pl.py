41990608119251faa5599338fe984d673ba865ab
miaumiaumiaux3 <tifachase@gmail.com>
2024-05-11 21:09:59 +0200

    changed polish sentence generator to GGUF because it's WAY faster (23s -> 3.6s)

--- generate_sentence_pl.py
+++ generate_sentence_pl.py
@@ -11,31 +11,31 @@
 import torch
 import warnings
 
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from ctransformers import AutoModelForCausalLM
+from transformers import AutoTokenizer, pipeline
 
 
 def load_model():
-    model_id = "speakleash/Bielik-7B-Instruct-v0.1-GPTQ"
-    tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
     model = AutoModelForCausalLM.from_pretrained(
-        model_id,
-        device_map="auto",
-        trust_remote_code=False,
-        revision="main"
+        "speakleash/Bielik-7B-Instruct-v0.1-GGUF",
+        model_file="bielik-7b-instruct-v0.1.Q4_K_M.gguf", # you can take different quantization resolution from speakleash/Bielik-7B-Instruct-v0.1-GGUF repo
+        model_type="mistral", gpu_layers=50, hf=True
     )
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        "speakleash/Bielik-7B-Instruct-v0.1", use_fast=True
+    )
+
     pipe = pipeline(model=model, tokenizer=tokenizer, task='text-generation', return_full_text=False)
-    gptq = True #should this be global?
 
     return pipe
 
-def prompt_model(pipe, human_prompt, t = 120) -> str:
+def prompt_model(pipe, human_prompt, temp=0.1) -> str:
     prompt = f"<s>[INST]{human_prompt}[/INST]"
     outputs = pipe(
         prompt,
-        max_new_tokens=t,
-        do_sample=True,
-        temperature=0.1, #0.1 is the default, changing it to .5 leads to... German xD
-        top_p=0.95,
+        max_new_tokens=120,
+        temperature=temp, #0.1 is the default, changing it to .5 leads to... German xD
     )
     result = outputs[0]["generated_text"]
     return result.strip(' "\'\t\r\n')
@@ -48,6 +48,9 @@ def generate_sample_sentence(pipe, word):
 
 #For running as a solo script to play with a model
 if __name__ == "__main__":
+    import time
+    from datetime import timedelta
+
     warnings.filterwarnings("ignore") #suppress warnings, since the output gets MESSY, comment out to debug issues
 
     print(f"CUDA is available: {torch.cuda.is_available()}")
@@ -56,17 +59,33 @@ if __name__ == "__main__":
     print(f"CUDA device index: {torch.cuda.current_device()}")
     print(f"CUDA device: {torch.cuda.get_device_properties(0)}")
     print(f"PyTorch version: {torch.__version__}")
+    print(torch.backends.cudnn.version())
+    print(torch.version.cuda)
     print("------------------------------------------------------------")
 
     pipe = load_model()
-    gptq = True
+
+    # while True:
+    #     human_prompt = input("Enter a prompt:\n")
+    #     if human_prompt.lower() == "exit":
+    #         exit()
+    #     if not human_prompt:
+    #         print("You wrote nothing, so I'm assuming you're done for now, goodbye!")
+    #         exit()
+    #     result = prompt_model(pipe, human_prompt)
+    #     print(result)
+    #
+
 
     while True:
-        human_prompt = input("Enter a prompt:\n")
-        if human_prompt.lower() == "exit":
-            exit()
-        if not human_prompt:
-            print("You wrote nothing, so I'm assuming you're done for now, goodbye!")
-            exit()
-        result = prompt_model(pipe, human_prompt)
+        #start timer
+        start_time = time.monotonic()
+        human_prompt = "Napisz zdanie, w którym znajduje się słowo 'taniec'"
+        result = prompt_model(pipe, human_prompt, temp=0.3)
         print(result)
+        #end timer
+        end_time = time.monotonic()
+        print('Runtime: ' + str(timedelta(seconds=end_time - start_time)))
+
+
+
